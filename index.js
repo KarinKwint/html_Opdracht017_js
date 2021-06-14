121622c7e3021706c639a84693a5c655deb446c9
KarinKwint <83405911+KarinKwint@users.noreply.github.com>
2021-06-14 22:14:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const makeApizza = function () {
+    console.log('make a dough');
+    console.log('let the dough rise');
+    console.log('put the ingredients on the folded dough');
+    console.log('bake the pizza');
+};
+
+makeApizza();
+makeApizza();
+
+const makeSushi = function() {
+    console.log('cook the rise');
+    console.log('put the rise and ingredients together');
+    console.log('roll the sushi');
+}
+makeSushi();
+makeSushi();
+makeSushi();
